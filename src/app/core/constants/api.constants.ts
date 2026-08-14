37ac66312320bbebfe92cb0251d4b30aac58f23f
Suryank <suryankmishra08@gmail.com>
2026-08-14 09:07:14 +0530

    api fix

--- src/app/core/constants/api.constants.ts
+++ src/app/core/constants/api.constants.ts
@@ -1,2 +1,2 @@
-// export const API_BASE = 'https://tms-backend-api-rd6j.onrender.com/api';
-export const API_BASE = 'http://localhost:8080/api';
+export const API_BASE = 'https://tms-backend-api-rd6j.onrender.com/api';
+// export const API_BASE = 'http://localhost:8080/api';
